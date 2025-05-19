9a9edabcd7f059af2a3f80c2dcd94a272e9c8671
kyo1941 <kyo1941@outlook.jp>
2025-05-20 08:42:56 +0900

    fix: Linkタグに変更

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,15 +1,16 @@
 import React from "react";
+import Link from 'next/link'
 import styles from "./header.module.css";
 
 export default function Header() {
   return (
     <header className={styles["header-root"]}>
-      <a href="/" className={styles["header-title"]}>kyo1941</a>
+      <Link href="/" className={styles["header-title"]}>kyo1941</Link>
       <nav>
         <ul className={styles["header-nav-list"]}>
-          <li><a href="#" className={styles["header-nav-link"]}>自己紹介</a></li>
-          <li><a href="#" className={styles["header-nav-link"]}>ブログ</a></li>
-          <li><a href="#" className={styles["header-nav-link"]}>ポートフォリオ</a></li>
+          <li><Link href="#" className={styles["header-nav-link"]}>自己紹介</Link></li>
+          <li><Link href="#" className={styles["header-nav-link"]}>ブログ</Link></li>
+          <li><Link href="#" className={styles["header-nav-link"]}>ポートフォリオ</Link></li>
         </ul>
       </nav>
     </header>
