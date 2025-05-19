7d9e85d12471f17749bce285f42ee15a7e413ff6
kyo1941 <kyo1941@outlook.jp>
2025-05-20 08:49:52 +0900

    fix: ヘッダーのリンクは変数で用意

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -3,14 +3,24 @@ import Link from 'next/link'
 import styles from "./header.module.css";
 
 export default function Header() {
+  const navItems = [
+    { id: 'profile', link: '#', label: '自己紹介' },
+    { id: 'blog', link: '#', label: 'ブログ' },
+    { id: 'portfolio', link: '#', label: 'ポートフォリオ' },
+  ];
+  
   return (
     <header className={styles["header-root"]}>
       <Link href="/" className={styles["header-title"]}>kyo1941</Link>
       <nav>
         <ul className={styles["header-nav-list"]}>
-          <li><Link href="#" className={styles["header-nav-link"]}>自己紹介</Link></li>
-          <li><Link href="#" className={styles["header-nav-link"]}>ブログ</Link></li>
-          <li><Link href="#" className={styles["header-nav-link"]}>ポートフォリオ</Link></li>
+          {navItems.map(item => (
+            <li key={item.id}>
+              <Link href={item.link} className={styles["header-nav-link"]}>
+                {item.label}
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
