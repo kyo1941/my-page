5a1914bd0395f3b999681ed8325890b576937b36
kyo1941 <kyo1941@outlook.jp>
2025-05-20 15:32:51 +0900

    feat: 画像にalt分を追加

--- src/app/components/ProfileSection.tsx
+++ src/app/components/ProfileSection.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image';
 import styles from "../page.module.css";
 
 export default function ProfileSection() {
@@ -6,7 +7,7 @@ export default function ProfileSection() {
     <section className={`${styles["top-section"]} ${styles["profile-section"]}`}> 
       <h3 className={styles["profile-title"]}>自己紹介</h3>
       <div className={styles["profile-flex"]}>
-        <img className={styles["profile-img"]} src="/profile.jpg"/>
+        <Image className={styles["profile-img"]} src="/profile.jpg" alt="プロフィール画像"/>
         <div className={styles["profile-content"]}>
           <p className={styles["profile-desc"]}>
             こんにちは、kyo1941です。<br />
