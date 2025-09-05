d0e63d3c140143daae65ea59d0a3906a05c9a95c
kyo1941 <kyo1941@outlook.jp>
2025-09-05 21:54:57 +0900

    feat: 経歴情報を更新

--- src/app/components/profile/CareerSection.tsx
+++ src/app/components/profile/CareerSection.tsx
@@ -9,12 +9,12 @@ export default function CareerSection() {
         {careerData.map((career, index) => (
           <div key={index} className="mb-10 ml-6">
             
-            <span className="absolute flex items-center justify-center w-12 h-12 bg-gray-50 p-1 rounded-full -left-6 ring-2 ring-white">
+            <span className="absolute flex items-center justify-center w-13 h-13 bg-white p-0.5 rounded-full -left-7 ring-2 ring-gray-100">
               <Image
                 src={career.icon}
                 alt={`${career.company} icon`}
-                width={48}
-                height={48}
+                width={64}
+                height={64}
                 className="object-contain rounded-full"
               />
             </span>
@@ -22,9 +22,9 @@ export default function CareerSection() {
             <div className="ml-4 pt-2">
               <h3 className="text-2xl font-semibold text-gray-900">{career.company}</h3>
               <p className="block my-3 text-sm font-normal leading-none text-gray-500">{career.period} | {career.position}</p>
-              <ul className="list-disc list-inside text-gray-700 space-y-1.5">
+              <ul className="list-disc list-outside text-gray-700 space-y-1.5">
                 {career.tasks.map((task, taskIndex) => (
-                  <li key={taskIndex}>{task}</li>
+                  <li key={taskIndex} className='whitespace-pre-line'>{task}</li>
                 ))}
               </ul>
               <div className="my-4 flex flex-wrap gap-2">
