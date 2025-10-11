96b1acfeb346a842fbda74ca8239090f2beee836
kyo1941 <kyo1941@outlook.jp>
2025-10-12 03:53:08 +0900

    refactor: 不要なコメントを削除

--- frontend/src/app/hooks/top/useBlogListTop.ts
+++ frontend/src/app/hooks/top/useBlogListTop.ts
@@ -1,7 +1,6 @@
 import { getSortedPostsData } from '@/app/repository/blogRepository';
 
 export function useBlogListTop(limit: number = 3) {
-  // Repositoryから取得し、必要ならslice等で整形
   const blogs = getSortedPostsData().slice(0, limit);
   return { blogs };
 }
