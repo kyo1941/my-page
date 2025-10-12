5ffc29f21baf77a2f98e28c9390b8d57dcfb0392
kyo1941 <kyo1941@outlook.jp>
2025-10-13 06:44:16 +0900

    feat: BlogControllerに必要なインポートを追加

--- backend/app/src/main/kotlin/my/backend/controller/BlogController.kt
+++ backend/app/src/main/kotlin/my/backend/controller/BlogController.kt
@@ -1,5 +1,12 @@
 package my.backend.controller
 
+import my.backend.dto.BlogDto
+import my.backend.service.BlogService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
 @RestController
 @RequestMapping("/api/blogs")
 class BlogController(private val blogService: BlogService) {
