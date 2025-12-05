32e8b07408f31bc1b78d2fb875f8ca910bc1a268
kyo1941 <kyo1941@outlook.jp>
2025-12-05 18:47:51 +0900

    fix: URL設定を修正

--- backend/app/src/main/kotlin/my/backend/plugins/HTTP.kt
+++ backend/app/src/main/kotlin/my/backend/plugins/HTTP.kt
@@ -3,6 +3,7 @@ package my.backend.plugins
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.cors.routing.*
+import java.net.URI
 
 fun Application.configureHTTP() {
     val allowedOrigins =
@@ -17,10 +18,13 @@ fun Application.configureHTTP() {
         allowCredentials = true
 
         allowedOrigins.split(",").forEach { origin ->
-            allowHost(
-                    origin.trim().removePrefix("http://").removePrefix("https://"),
-                    schemes = listOf("http", "https")
-            )
+            val uri = URI(origin.trim())
+            val host = uri.host
+            val scheme = uri.scheme
+
+            if (host != null && scheme != null) {
+                allowHost(host, schemes = listOf(scheme))
+            }
         }
     }
 }
