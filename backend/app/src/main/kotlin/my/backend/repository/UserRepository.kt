cf2531d5780b97fa3d3b664a3edd4de72fb3610f
kyo1941 <kyo1941@outlook.jp>
2026-01-31 20:08:38 +0900

    フォーマット

--- backend/app/src/main/kotlin/my/backend/repository/UserRepository.kt
+++ backend/app/src/main/kotlin/my/backend/repository/UserRepository.kt
@@ -3,7 +3,6 @@ package my.backend.repository
 import my.backend.db.schema.UserTable
 import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 
