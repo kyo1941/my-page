46fda0a6cd86242aa0e3d4fe2878f720bebba1b3
kyo1941 <kyo1941@outlook.jp>
2026-02-07 19:32:53 +0900

    ktlintの警告の回避

--- backend/app/src/main/kotlin/db/migration/V2__InsertAdminUser.kt
+++ backend/app/src/main/kotlin/db/migration/V2__InsertAdminUser.kt
@@ -4,14 +4,16 @@ import org.flywaydb.core.api.migration.BaseJavaMigration
 import org.flywaydb.core.api.migration.Context
 import org.mindrot.jbcrypt.BCrypt
 
-@Suppress("unused")
+@Suppress("unused", "ClassName")
 class V2__InsertAdminUser : BaseJavaMigration() {
     override fun migrate(context: Context) {
         val username = System.getenv("ADMIN_USERNAME")
         val password = System.getenv("ADMIN_PASSWORD")
 
         if (username.isNullOrBlank() || password.isNullOrBlank()) {
-            throw RuntimeException("Environment variables ADMIN_USERNAME and ADMIN_PASSWORD must be set for migration V2.")
+            throw RuntimeException(
+                "Environment variables ADMIN_USERNAME and ADMIN_PASSWORD must be set for migration V2.",
+            )
         }
 
         val hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt())
@@ -23,4 +25,3 @@ class V2__InsertAdminUser : BaseJavaMigration() {
         }
     }
 }
-
