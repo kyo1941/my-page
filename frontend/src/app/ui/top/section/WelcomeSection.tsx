41e8d353d288f6207c30c9fa5a5dd2fae8352ef0
kyo1941 <kyo1941@outlook.jp>
2026-02-14 17:08:05 +0900

    feat: 文字列の表示アニメーションを調整

--- frontend/src/app/ui/top/section/WelcomeSection.tsx
+++ frontend/src/app/ui/top/section/WelcomeSection.tsx
@@ -4,21 +4,40 @@ import { motion } from "framer-motion";
 import { useState, useEffect } from "react";
 
 export default function WelcomeSection() {
-  const text = "Hello, I'm kyo1941.";
   const [displayText, setDisplayText] = useState("");
+  const [isTypingComplete, setIsTypingComplete] = useState(false);
 
   useEffect(() => {
-    let index = 0;
-    const timer = setInterval(() => {
-      if (index < text.length) {
-        const char = text.charAt(index);
-        setDisplayText((prev) => prev + char);
-        index++;
-      } else {
-        clearInterval(timer);
+    /**
+     * NOTE: 再レンダリング時に一文字目が重複されて表示されたため明示的にリセットする
+     */
+    setDisplayText("");
+
+    const texts = ["Hello,", " I'm kyo1941."];
+    let isMounted = true;
+
+    const typeText = async () => {
+      const wait = (ms: number) =>
+        new Promise((resolve) => setTimeout(resolve, ms));
+
+      for (const segment of texts) {
+        for (const char of segment) {
+          if (!isMounted) return;
+          setDisplayText((prev) => prev + char);
+          await wait(120);
+        }
+
+        if (!isMounted) return;
+        await wait(65);
       }
-    }, 100);
-    return () => clearInterval(timer);
+      if (isMounted) setIsTypingComplete(true);
+    };
+
+    typeText();
+
+    return () => {
+      isMounted = false;
+    };
   }, []);
 
   return (
@@ -33,8 +52,8 @@ export default function WelcomeSection() {
       </h2>
       <motion.p
         initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 3, duration: 0.8 }}
+        animate={isTypingComplete ? { opacity: 1, y: 0 } : {}}
+        transition={{ duration: 0.8, delay: 0.4 }}
         className="text-sm sm:text-lg text-gray-500 mt-4 font-mono"
       >
         Android Engineer / Mapo tofu Developer
