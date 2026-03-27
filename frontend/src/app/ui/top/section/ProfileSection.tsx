9b140e2a791ad95b7cbc331c1fb4edd252c7d660
kyo1941 <kyo1941@outlook.jp>
2026-03-27 13:39:58 +0900

    自己紹介文の微修正

--- frontend/src/app/ui/top/section/ProfileSection.tsx
+++ frontend/src/app/ui/top/section/ProfileSection.tsx
@@ -19,7 +19,7 @@ export default function ProfileSection() {
           <p className="py-4 text-gray-900">
             こんにちは、kyo1941です。
             <br />
-            Androidエンジニアをやっています。音楽を聴いたり、料理をしたりします。
+            Androidエンジニアをやっています。音楽を聴いたり、麻婆豆腐を作ったりしています。
             <br />
           </p>
           <div>
